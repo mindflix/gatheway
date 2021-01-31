673a478fe236f8158f711ed24b9bfbb17cc2cb96
mindflix <nyco.demol@gmail.com>
2021-01-31 18:13:36 +0100

    +sass +webpack-encore

--- src/Controller/WardrobeController.php
+++ src/Controller/WardrobeController.php
@@ -6,15 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class WardrobeController extends AbstractController
 {
     /**
-     * @Route("/", name="root")
+     * @Route("/wardrobe", name="wardrobe")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->redirectToRoute('root');
     }
 }
