8c4f1640b20feb76bd13e7596af4033690b315d1
mindflix <nyco.demol@gmail.com>
2021-02-01 02:43:02 +0100

    responsiveless navbar

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,10 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
 // any CSS you import will output into a single css file (app.css in this case)
-import "./styles/app.scss";
+import "./styles/main.sass";
 
-
-console.log("Hello Webpack Encore !");
+// append images to build
+const imagesContext = require.context(
+  "./images",
+  true,
+  /\.(png|jpg|jpeg|gif|ico|svg|webp)$/
+);
+imagesContext.keys().forEach(imagesContext);
