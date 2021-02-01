893e6d4c72a7c70f09e126c71860cf2f4a7ef70c
mindflix <nyco.demol@gmail.com>
2021-02-01 21:15:46 +0100

    nav scroll js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,5 +1,32 @@
 const menuBtn = document.getElementById("menu-btn");
 const menuSidebar = document.getElementById("menu-sidebar");
+const navLinks = document.getElementById("nav-links");
+
+let lastPosition = 0;
+let difference = 0;
+let stateLinks = true;
+
+setInterval(() => {
+  if (window.scrollY !== lastPosition) {
+    difference = lastPosition - window.scrollY;
+    lastPosition = window.scrollY;
+
+    if (difference > 10) {
+      navLinks.classList.remove("disable");
+      stateLinks = true;
+    }
+    if (difference < -10) {
+      navLinks.classList.add("disable");
+      stateLinks = false;
+    }
+    if (Math.abs(difference) < 10 && stateLinks === true) {
+      navLinks.classList.remove("disable");
+    }
+    if (Math.abs(difference) < 10 && stateLinks === false) {
+      navLinks.classList.add("disable");
+    }
+  }
+}, 100);
 
 menuBtn.addEventListener("click", handleMenu);
 
