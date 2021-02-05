7f589e4b0d634c26aa6282d88abc80c402bce98b
mindflix <nyco.demol@gmail.com>
2021-02-05 17:10:09 +0100

    parallax effect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
+import section1 from "../images/section1.jpg";
+import section2 from "../images/section2.jpg";
 
 function Home() {
+  const [offsetY,setOffsetY] = useState(0)
+
+  function handleScroll() {
+    setOffsetY(window.pageYOffset)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
     <div className="l-viewport">
-      <div className="l-section">
-        <div className="l-content"></div>
-      </div>
-      <div className="container">
-        <label htmlFor="nom"></label>
-      </div>
+        <img src={section1} className="parallax" style={{ transform: `translate3d(0,${-offsetY*0.01}vh,0)` }} />
+        <img src={section2} className="parallax" style={{ transform: `translate3d(0,${-offsetY*0.06 - 30}vh,0)` }}/>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,46 +8,20 @@ import {
 
 function Nav() {
   const [isActive, setActive] = useState(false);
-  const [isTop, setTop] = useState(true);
-  const [scrollY, setScrollY] = useState(0);
 
-  const stopScroll = () => {
+  function stopScroll() {
     const body = document.querySelector("body");
     if (isActive) {
       body.style.overflow = "visible";
     } else {
       body.style.overflow = "hidden";
     }
-  };
-
-  const handleMenu = () => {
-    setActive(!isActive);
-    stopScroll();
-  };
-  let security = false;
-
-  function logit() {
-    let diff = window.pageYOffset - scrollY;
-    if (diff > 5 && security === false) {
-      setTop(false);
-    }
-    if (diff < -5 && security === false) {
-      setTop(true);
-    }
-    if (Math.abs(diff) <= 1) {
-      security = true;
-    } else {
-      security = false;
-    }
-    setScrollY(window.pageYOffset);
   }
 
-  useEffect(() => {
-    window.addEventListener("scroll", logit);
-    return () => {
-      window.removeEventListener("scroll", logit);
-    };
-  });
+  function handleMenu() {
+    setActive(!isActive);
+    stopScroll();
+  }
 
   return (
     <React.Fragment>
@@ -74,50 +48,83 @@ function Nav() {
             </li>
           </ul>
         </div>
-        <div className={isTop ? "nav--links" : "nav--links disable"}>
-          <li>
-            <span>Femme</span>
-          </li>
-          <li>
-            <span>Homme</span>
-          </li>
-          <li>
-            <span>Haute Couture</span>
-          </li>
-          <li>
-            <span>Collections</span>
-          </li>
-          <li>
-            <span>La Maison</span>
-          </li>
-          <li>
-            <span>Contact</span>
-          </li>
-        </div>
+        <Links />
       </nav>
-      <div className={isActive ? "menu active" : "menu"}>
-        <div className="menu--links">
-          <li>
-            <span>Femme</span>
-          </li>
-          <li>
-            <span>Homme</span>
-          </li>
-          <li>
-            <span>Haute Couture</span>
-          </li>
-          <li>
-            <span>Collections</span>
-          </li>
-          <li>
-            <span>La Maison</span>
-          </li>
-          <li>
-            <span>Contact</span>
-          </li>
-        </div>
-      </div>
+      <Menu isActive={isActive} />
     </React.Fragment>
+  );
+}
+
+function Links() {
+  const [isTop, setTop] = useState(true);
+  const [scrollY, setScrollY] = useState(0);
+
+  function handleNav() {
+    let diff = window.pageYOffset - scrollY;
+
+    if (diff > 5 && diff >= 1) {
+      setTop(false);
+    }
+    if ((diff < -5 && diff <= -1) || window.pageYOffset === 0) {
+      setTop(true);
+    }
+    setScrollY(window.pageYOffset);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleNav);
+    return () => {
+      window.removeEventListener("scroll", handleNav);
+    };
+  });
+  return (
+    <div className={isTop ? "nav--links" : "nav--links disable"}>
+      <li>
+        <span>Femme</span>
+      </li>
+      <li>
+        <span>Homme</span>
+      </li>
+      <li>
+        <span>Haute Couture</span>
+      </li>
+      <li>
+        <span>Collections</span>
+      </li>
+      <li>
+        <span>La Maison</span>
+      </li>
+      <li>
+        <span>Contact</span>
+      </li>
+    </div>
+  );
+}
+
+function Menu(props) {
+  return (
+    <div className={props.isActive ? "menu active" : "menu"}>
+      <div className="menu--links">
+        <li>
+          <span>Femme</span>
+        </li>
+        <li>
+          <span>Homme</span>
+        </li>
+        <li>
+          <span>Haute Couture</span>
+        </li>
+        <li>
+          <span>Collections</span>
+        </li>
+        <li>
+          <span>La Maison</span>
+        </li>
+        <li>
+          <span>Contact</span>
+        </li>
+      </div>
+    </div>
   );
 }
 
