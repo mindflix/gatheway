6a03c5da5c64654293abd370285f1acf7b1c3f64
mindflix <nyco.demol@gmail.com>
2021-02-06 03:20:49 +0100

    fixed parallax + grid article

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,109 @@
 import React, { useState, useEffect } from "react";
-import section1 from "../images/section1.jpg";
-import section2 from "../images/section2.jpg";
+
+import section2 from "../images/section4.jpg";
+import section1 from "../images/section3mobile.jpg";
+import section3 from "../images/section3.jpg";
+import article from "../images/article.jpg";
 
 function Home() {
-  const [offsetY,setOffsetY] = useState(0)
+  const [offsetY, setOffsetY] = useState(0);
+  const [isMobile, setMobile] = useState(undefined);
 
   function handleScroll() {
-    setOffsetY(window.pageYOffset)
+    setOffsetY(window.pageYOffset);
+  }
+
+  function handleScreen() {
+    if (window.innerWidth > 1180) {
+      setMobile(false);
+    } else {
+      setMobile(true);
+    }
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    handleScreen();
+  }, []);
 
   return (
-    <div className="l-viewport">
-      <div className="parallax">
-        <img src={section1} style={{ transform: `translate3d(0,${-offsetY *0.01}vh,0)` }} alt="section1" />
-        <img src={section2} style={{ transform: `translate3d(0,${-offsetY * 0.06 }vh,0)`, top: "-30px" }} alt="section1"/>
+    <React.Fragment>
+      <div
+        className="parallax"
+        style={{ top: `${offsetY * 0.02}vh`, zIndex: 1 }}
+      >
+        <div className="parallax--bg">
+          <img src={isMobile ? section1 : section3} alt="section1" />
+        </div>
+        <div
+          className="parallax--content"
+          style={{ transform: `translate3d(0,${-offsetY * 0.02}vh,0)` }}
+        >
+          <div className="section"></div>
+          <div className="viewport">
+            <div className="container">
+              <div className="text--banner">
+                <h1>#nouvelle collection</h1>
+              </div>
+              <div className="wrapper">
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <div
+        className="parallax"
+        style={{ top: `${-offsetY * 0.005}vh`, zIndex: 0 }}
+      >
+        <div className="parallax--bg">
+          <img src={section2} alt="section1" />
+        </div>
+        <div
+          className="parallax--content"
+          style={{ transform: `translate3d(0,${10 - offsetY * 0.02}vh,0)` }}
+        >
+          <div className="section"></div>
+          <div className="viewport">
+            <div className="container">
+              <div className="text--banner">
+                <h1>La Sélection Printemps</h1>
+              </div>
+              <div className="wrapper">
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+              </div>
+            </div>
+            <div className="text--banner">
+              <h1>Notre favori</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function Article() {
+  return (
+    <div className="wrapper--article">
+      <img src={article} alt="article" />
+      <h3>Balenciaga</h3>
     </div>
   );
 }
